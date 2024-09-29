99ea78302d38783a9cf9109dc08f55d2f290c5f4
0384182069 <phuc.ph24012004@gmail.comm>
2024-09-29 23:15:40 +0700

    setup firebase_core,firebase_auth,cloud_firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:ecommerce_app/pages/bottommav.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb){
+    await Firebase.initializeApp(
+      options: const FirebaseOptions( 
+        apiKey: "AIzaSyAZMKwhpvBcOomAzIbw5zn1DMS_vedh1vw",
+        authDomain: "ecommerceapp-5a835.firebaseapp.com",
+        projectId: "ecommerceapp-5a835",
+        storageBucket: "ecommerceapp-5a835.appspot.com",
+        messagingSenderId: "710412569298",
+        appId: "1:710412569298:web:9d9ae0a2fbf37f0a29fb0b",
+        measurementId: "G-1BS0SRZ0FG"));
+  }else{
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 
 }
@@ -12,7 +28,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'SPEEDYEATS Fast Food',
+      title: 'SPEEDYEATS',
       home: BottomNav()
     );
   }
